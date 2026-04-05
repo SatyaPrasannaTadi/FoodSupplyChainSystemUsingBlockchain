5b365bef4c8e9f00c946c985519a26aaf5ddb73b
SatyaPrasannaTadi <satyaprasanna2356@gmail.com>
2026-04-05 17:31:08 +0530

    Updated correct Vercel URL

--- food-supply-blokchain/backend/index.js
+++ food-supply-blokchain/backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Configure CORS to allow requests from frontend
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://food-supply-chain-system-using-bloc.vercel.app'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://food-supply-chain-system-using-blockchain-30itxzcte.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
